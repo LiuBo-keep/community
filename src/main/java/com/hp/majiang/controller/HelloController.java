eabfe95f8b59a1d7c8525759735ddc447a24c349
liu <1712624418@qq.com>
2020-06-13 11:45:11 +0800

    community

---
+++ src/main/java/com/hp/majiang/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.hp.majiang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author 17126
+ * @Date 2020/6/13 11:25
+ */
+
+@Controller
+public class HelloController {
+
+
+    @GetMapping("hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
